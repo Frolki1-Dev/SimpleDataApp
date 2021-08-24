eea5bc7b82b50f8c5395d548480f03748cc3bd3c
Frank Giger <18479991+Frolki1-Dev@users.noreply.github.com>
2021-08-24 19:35:17 +0200

    Projektdateien hinzufügen.

---
+++ SimpleDataApp/Navigation.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace SimpleDataApp
+{
+    partial class Navigation
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGoToAdd = new System.Windows.Forms.Button();
+            this.btnGoToFillOrCancel = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(295, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Was möchtest du machen?";
+            // 
+            // btnGoToAdd
+            // 
+            this.btnGoToAdd.Location = new System.Drawing.Point(18, 72);
+            this.btnGoToAdd.Name = "btnGoToAdd";
+            this.btnGoToAdd.Size = new System.Drawing.Size(290, 41);
+            this.btnGoToAdd.TabIndex = 1;
+            this.btnGoToAdd.Text = "Account erstellen";
+            this.btnGoToAdd.UseVisualStyleBackColor = true;
+            this.btnGoToAdd.Click += new System.EventHandler(this.btnGoToAdd_Click);
+            // 
+            // btnGoToFillOrCancel
+            // 
+            this.btnGoToFillOrCancel.Location = new System.Drawing.Point(18, 119);
+            this.btnGoToFillOrCancel.Name = "btnGoToFillOrCancel";
+            this.btnGoToFillOrCancel.Size = new System.Drawing.Size(290, 41);
+            this.btnGoToFillOrCancel.TabIndex = 2;
+            this.btnGoToFillOrCancel.Text = "Bestellung erstellen oder stornieren";
+            this.btnGoToFillOrCancel.UseVisualStyleBackColor = true;
+            this.btnGoToFillOrCancel.Click += new System.EventHandler(this.btnGoToFillOrCancel_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(238, 197);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Beenden";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Navigation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 235);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGoToFillOrCancel);
+            this.Controls.Add(this.btnGoToAdd);
+            this.Controls.Add(this.label1);
+            this.Name = "Navigation";
+            this.Padding = new System.Windows.Forms.Padding(15);
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGoToAdd;
+        private System.Windows.Forms.Button btnGoToFillOrCancel;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
